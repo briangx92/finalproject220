5c1e6933bdcad99b42ce9c535c67a9e35b04ca36
Jonathan Marciano <bobthemoon@gmail.com>
2019-11-05 11:11:02 -0500

    - Login Table now created on initial startup

--- db.php
+++ db.php
@@ -8,6 +8,15 @@ $conn = new mysqli($dbServername, $dbUsername , $dbPassword);
 $sql = "CREATE DATABASE old_home";
 if ($conn->query($sql) === TRUE) {
     echo "Database created";
+    $dbName = "old_home";
+    $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+    $sql = "CREATE TABLE Login (
+    User_ID int,
+    email varchar(35),
+    password varchar(255),
+    approved boolean
+);";
+$result = mysqli_query($conn, $sql);
 } else {
 }
 
