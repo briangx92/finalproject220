43b9a2d95894e128c7212bfaaea1109279e7ded6
Jonathan Marciano <bobthemoon@gmail.com>
2019-11-06 10:50:22 -0500

    - Application checks for email, password and approval - If user found, finds ID and runs on user table - Finds role and sorts user to correct page based on role

--- db.php
+++ db.php
@@ -10,13 +10,25 @@ if ($conn->query($sql) === TRUE) {
     echo "Database created";
     $dbName = "old_home";
     $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
-    $sql = "CREATE TABLE Login (
+    $sql = "CREATE TABLE login (
     User_ID int,
     email varchar(35),
-    password varchar(255),
+    pass varchar(255),
     approved boolean
 );";
 $result = mysqli_query($conn, $sql);
+$sql2 = "CREATE TABLE user(
+User_ID int,
+role varchar(35),
+fname varchar(255),
+lname varchar(255),
+phone int,
+dob date,
+family_code int,
+emergency_contact varchar(200),
+relation varchar(150)
+);";
+$result = mysqli_query($conn, $sql2);
 } else {
 }
 
