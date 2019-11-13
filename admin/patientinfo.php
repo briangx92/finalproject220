48e0501604baad8ec052443ffb0ca98628dda35b
Jonathan Marciano <bobthemoon@gmail.com>
2019-11-13 09:03:57 -0500

    Values of seached patient show up in table

--- admin/patientinfo.php
+++ admin/patientinfo.php
@@ -8,8 +8,19 @@ if ($_SESSION['role'] != 'admin') {
 ?>
 <?php
 
+$getpatientid = $_POST['getpatientid'] ?? '';
+$getpatientid = intval($getpatientid);
 
+$getpatient = "SELECT * FROM patient WHERE patientid = '$getpatientid'";
+$patientinfo = mysqli_query($conn, $getpatient);
+$wegood = mysqli_fetch_assoc($patientinfo);
+
+$nameid = $wegood['userid'];
+$nameget = "SELECT fname,lname FROM users WHERE userid = '$nameid'";
+$namegot = mysqli_query($conn, $nameget);
+$getgot = mysqli_fetch_assoc($namegot);
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -21,17 +32,18 @@ if ($_SESSION['role'] != 'admin') {
 <body>
 
 <!-- Patient  Information Form-->
-<form action="" method="POST">
+<form action="patientinfo.php" method="POST">
     <fieldset>
         <legend>Patient Information</legend>
         <p>
-        Patient ID: <input type="text" name="patientid">
+        Patient ID: <input type="text" name="getpatientid" value="<?php echo $wegood['patientid'];?>" >
+        <input type="submit">
         <br>
-        Group: <input type="text" name="group">
-        Admission Date<input type="date" name="admdate">
+        Group: <input type="text" name="group" value="<?php echo $wegood['group_num'];?>">
+        Admission Date<input type="date" name="admdate" value="<?php echo $wegood['admission_date'];?>">
 
-        <input type="date" name="date">
-        Patient Name: <input type="text" name="name" value="<?php // CODE FOR PATIENT NAME ?>">
+        Patient First Name: <input type="text" name="fname" value="<?php echo $getgot['fname']; ?>">
+        Patient Last Name: <input type="text" name="lname" value="<?php echo $getgot['lname']; ?>">
         </p>
         <select name="doctor">
             <?php
@@ -43,6 +55,7 @@ if ($_SESSION['role'] != 'admin') {
 
     </fieldset>
 </form>
+
 
 </body>
 </html>
