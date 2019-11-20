a884dcbac9ab11b19122620d2b7da35a004898f4
Jonathan Marciano <bobthemoon@gmail.com>
2019-11-20 10:42:41 -0500

    - Table of roles now fully edit-able - Database changes on click

--- role.php
+++ role.php
@@ -2,6 +2,94 @@
 include_once 'db.php';
 session_start();
 
+$admin = $_POST['admin'] ?? '';
+$patient = $_POST['patient'] ?? '';
+$family = $_POST['family'] ?? '';
+$doctor = $_POST['doctor'] ?? '';
+$supervisor = $_POST['supervisor'] ?? '';
+$caregiver = $_POST['caregiver'] ?? '';
+
+if( isset($_POST['admin']) )  {
+    $getinfo = "SELECT admin FROM role WHERE page = '$admin'" ;
+    $theirinfo = mysqli_query($conn, $getinfo);
+    $newinfo = mysqli_fetch_assoc($theirinfo);
+
+    $approval = ($newinfo['admin'] == 1 ? 1 : 0);
+    if ($approval == 1) {
+        $unapprove = "UPDATE role SET admin = 0 WHERE page = '$admin';";
+        mysqli_query($conn, $unapprove);
+    } else {
+        $approve = "UPDATE role SET admin = 1 WHERE page = '$admin';";
+        mysqli_query($conn, $approve);
+    }
+    header("Refresh:0");
+} elseif ( isset($_POST['patient']) ) {
+    $getinfo = "SELECT patient FROM role WHERE page = '$patient'" ;
+    $theirinfo = mysqli_query($conn, $getinfo);
+    $newinfo = mysqli_fetch_assoc($theirinfo);
+    $approval = ($newinfo['patient'] == 1 ? 1 : 0);
+    if ($approval == 1) {
+        $unapprove = "UPDATE role SET patient = 0 WHERE page = '$patient';";
+        mysqli_query($conn, $unapprove);
+    } else {
+        $approve = "UPDATE role SET patient = 1 WHERE page = '$patient';";
+        mysqli_query($conn, $approve);
+    }
+    header("Refresh:0");
+} elseif ( isset($_POST['family']) ) {
+    $getinfo = "SELECT family FROM role WHERE page = '$family'" ;
+    $theirinfo = mysqli_query($conn, $getinfo);
+    $newinfo = mysqli_fetch_assoc($theirinfo);
+    $approval = ($newinfo['family'] == 1 ? 1 : 0);
+    if ($approval == 1) {
+        $unapprove = "UPDATE role SET family = 0 WHERE page = '$family';";
+        mysqli_query($conn, $unapprove);
+    } else {
+        $approve = "UPDATE role SET family = 1 WHERE page = '$family';";
+        mysqli_query($conn, $approve);
+    }
+    header("Refresh:0");
+} elseif ( isset($_POST['doctor']) ) {
+    $getinfo = "SELECT doctor FROM role WHERE page = '$doctor'" ;
+    $theirinfo = mysqli_query($conn, $getinfo);
+    $newinfo = mysqli_fetch_assoc($theirinfo);
+    $approval = ($newinfo['doctor'] == 1 ? 1 : 0);
+    if ($approval == 1) {
+        $unapprove = "UPDATE role SET doctor = 0 WHERE page = '$doctor';";
+        mysqli_query($conn, $unapprove);
+    } else {
+        $approve = "UPDATE role SET doctor = 1 WHERE page = '$doctor';";
+        mysqli_query($conn, $approve);
+    }
+    header("Refresh:0");
+} elseif ( isset($_POST['supervisor']) ) {
+    $getinfo = "SELECT supervisor FROM role WHERE page = '$supervisor'" ;
+    $theirinfo = mysqli_query($conn, $getinfo);
+    $newinfo = mysqli_fetch_assoc($theirinfo);
+    $approval = ($newinfo['supervisor'] == 1 ? 1 : 0);
+    if ($approval == 1) {
+        $unapprove = "UPDATE role SET supervisor = 0 WHERE page = '$supervisor';";
+        mysqli_query($conn, $unapprove);
+    } else {
+        $approve = "UPDATE role SET supervisor = 1 WHERE page = '$supervisor';";
+        mysqli_query($conn, $approve);
+    }
+    header("Refresh:0");
+} elseif ( isset($_POST['caregiver']) ) {
+    $getinfo = "SELECT caregiver FROM role WHERE page = '$caregiver'" ;
+    $theirinfo = mysqli_query($conn, $getinfo);
+    $newinfo = mysqli_fetch_assoc($theirinfo);
+    $approval = ($newinfo['caregiver'] == 1 ? 1 : 0);
+    if ($approval == 1) {
+        $unapprove = "UPDATE role SET caregiver = 0 WHERE page = '$caregiver';";
+        mysqli_query($conn, $unapprove);
+    } else {
+        $approve = "UPDATE role SET caregiver = 1 WHERE page = '$caregiver';";
+        mysqli_query($conn, $approve);
+    }
+    header("Refresh:0");
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -36,7 +124,7 @@ session_start();
                 <th>Supervisor</th>
                 <th>Caregiver</th>
             </tr>
-
+            <form action='role.php' method='post'>
 <?php
 
         $dir = getcwd();
@@ -53,28 +141,22 @@ session_start();
                 $getinfo = "SELECT * FROM role WHERE page = '$page'" ;
                 $theirinfo = mysqli_query($conn, $getinfo);
                 $newinfo = mysqli_fetch_assoc($theirinfo);
-                echo "<form action='role.php' method='post'>";
                 echo "<tr>";
                 echo "<td> $page </td>";
-                echo "<td><button type='submit' value='$page' name='admin'>" . ($newinfo['admin'] == 1 ? 'True' : 'False') . "</button></td>";
-                echo "<td><button type='submit' value='$page' name='patient'>" . ($newinfo['patient'] == 1 ? 'True' : 'False') . "</button></td>";
-                echo "<td><button type='submit' value='$page' name='family'>" . ($newinfo['family'] == 1 ? 'True' : 'False') . "</button></td>";
-                echo "<td><button type='submit' value='$page' name='doctor'>" . ($newinfo['doctor'] == 1 ? 'True' : 'False') . "</button></td>";
-                echo "<td><button type='submit' value='$page' name='supervisor'>" . ($newinfo['supervisor'] == 1 ? 'True' : 'False') . "</button></td>";
-                echo "<td><button type='submit' value='$page' name='caregiver'>" . ($newinfo['caregiver'] == 1 ? 'True' : 'False') . "</button></td>";
+                echo "<td><button type='submit' value='$page' name='admin'>" . ($newinfo['admin'] == 1 ? 'Approved' : 'Denied') . "</button></td>";
+                echo "<td><button type='submit' value='$page' name='patient'>" . ($newinfo['patient'] == 1 ? 'Approved' : 'Denied') . "</button></td>";
+                echo "<td><button type='submit' value='$page' name='family'>" . ($newinfo['family'] == 1 ? 'Approved' : 'Denied') . "</button></td>";
+                echo "<td><button type='submit' value='$page' name='doctor'>" . ($newinfo['doctor'] == 1 ? 'Approved' : 'Denied') . "</button></td>";
+                echo "<td><button type='submit' value='$page' name='supervisor'>" . ($newinfo['supervisor'] == 1 ? 'Approved' : 'Denied') . "</button></td>";
+                echo "<td><button type='submit' value='$page' name='caregiver'>" . ($newinfo['caregiver'] == 1 ? 'Approved' : 'Denied') . "</button></td>";
                 echo "</tr>";
-                echo "</form>";
+
             }
         }
 
-        $admin = $_POST['admin'] ?? '';
-        $patient = $_POST['patient'] ?? '';
-        $family = $_POST['family'] ?? '';
-        $doctor = $_POST['doctor'] ?? '';
-        $supervisor = $_POST['supervisor'] ?? '';
-        $caregiver = $_POST['caregiver'] ?? '';
 
 ?>
+</form>
 </body>
 
 </html>
