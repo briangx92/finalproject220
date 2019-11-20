b0b06f3a9aaf351ca387820cf0f2a4969e8391ff
Brian Gomez <briangx92@gmail.com>
2019-11-20 10:46:02 -0500

    Checkpoint

--- doctappt.php
+++ doctappt.php
@@ -1,28 +1,43 @@
 <?php
 include_once 'db.php';
+session_start();
 ?>
 <?php
 
 
 
-// Posts
-$apt_date = $_POST['date'] ?? '';
+// POSTS
 $patientid = $_POST['patid'] ?? '';
+$apt_date = $_POST['date'] ?? '';
+$docid = $_POST['doctorid'] ?? '';
 
 
-// Queries
-$sql_doctor = "SELECT fname, lname FROM users;";
+// SQL Variable Queries
+
+$sql_doctor = "SELECT fname, lname FROM users WHERE role = 'doctor';";
+
+$sql_patient = "SELECT * FROM users u JOIN doctor_appt d ON u.userid = d.patientid WHERE d.patientid = '$patientid';";
+
 $sql_date = "SELECT * FROM users u JOIN doctor_appt d ON u.userid = d.doctorid WHERE d.apt_date = '$apt_date';";
-$sql_user = "SELECT * FROM users u JOIN doctor_appt d ON u.userid = d.patientid WHERE d.patientid = '$patientid';";
 
-// User Query
-$sql_query_user = mysqli_query($conn, $sql_user);
-$result_user = mysqli_fetch_assoc($sql_query_user);
+$sql_new_appt = "INSERT INTO doct_appt (patientid, doctorid, apt_date)
+VALUES ($patientid, $docid, $apt_date);";
+
+// Doctor Query
+$sql_doc_query = mysqli_query($conn, $sql_doctor);
+$result_doctor = mysqli_fetch_assoc($sql_doc_query);
+
+// Patient Query
+$sql_query_patient = mysqli_query($conn, $sql_patient);
+$result_patient = mysqli_fetch_assoc($sql_query_patient);
 
 // Date Query
 $sql_query_date = mysqli_query($conn, $sql_date);
 $result_date = mysqli_fetch_assoc($sql_query_date);
 
+// Insert Query
+$sql_query_insert = mysqli_query($conn, $sql_new_appt);
+$result_insert = mysqli_fetch_assoc($sql_query_insert);
 
 ?>
 
@@ -41,28 +56,30 @@ $result_date = mysqli_fetch_assoc($sql_query_date);
         <fieldset>
             <legend>Doctors Appointment</legend>
             <label>Patient ID</label>
-            <input type="text" name="patid" value="<?php echo "{$result_date['patientid']}"; ?>">
+            <input type="text" name="patid" value="<?php ?>">
             <br>
-            <label>Patient: <?php echo "{$result_user['fname']} {$result_user['lname']}"; ?></label>
+            <label>Patient: <?php echo "{$result_patient['fname']} {$result_patient['lname']}"; ?></label>
             <br>
             <label>Date</label>
-            <input type="date" name="date" value="<?php  echo $apt_date; ?>">
+            <input type="date" name="date" value="<?php  ?>">
             <label>Doctor</label>
             <select name="doctor">
                 <?php
-           
-                echo "<option>{$result_date['fname']} {$result_date['lname']}</option>";
+                if($result_doctor > 0) {
+                    while($row = mysqli_fetch_assoc($sql_query_user)) {
+                        echo "<option>{$row['fname']} {$row['lname']}</option>";
+                    }
+                }
                 ?>
             </select>
+            <br>
+            
             <button type="submit" value="submit">Submit</button>
+
         </fieldset>
+
     </form>
 
 </body>
 
 </html>
-
-<?php
-
-
-?>
