2ef92cc3bbda15c9848d02109c6261cdb1d7be80
Jonathan Marciano <bobthemoon@gmail.com>
2019-11-21 11:26:06 -0500

    - Page now gives total due for entered patient

--- payment.php
+++ payment.php
@@ -19,6 +19,11 @@ $date = date_create(date('Y/m/d'));
 $interval = date_diff($admindate, $date);
 $date_difference = $interval->format('%a');
 
+$appointment_count = "SELECT count(patientid) FROM doctor_appt WHERE patientid = '$getpatientid'";
+$numgot = mysqli_query($conn, $appointment_count);
+$appointno = mysqli_fetch_assoc($numgot);
+$numnappoint = $appointno['count(patientid)'];
+$thecost = ($numnappoint * 50) + ($date_difference * 10);
 ?>
 <?php
 
@@ -46,7 +51,7 @@ $date_difference = $interval->format('%a');
         Patient Name: <?php echo $getgot['fname'] . ' ' . $getgot['lname']; ?>
         <br>
         <label>Total Due: $</label>
-        <input type="text" name="due" value=" <?php // sql variable ?>" disabled>
+        <input type="text" name="due" value=" <?php echo $thecost ?>" disabled>
         <br>
         <label>New Payment: $</label>
         <input type="text" name="pay" placeholder="0.00" value="<?php // sql variable for insert ?>">
