e28d48df0fe22337c837f83c1a4f8df47a267463
Brian Gomez <briangx92@gmail.com>
2019-11-27 10:50:02 -0500

    updated DB security

--- db.php
+++ db.php
@@ -5,6 +5,8 @@ $dbUsername = "root";
 $dbPassword = "";
 $conn = new mysqli($dbServername, $dbUsername , $dbPassword);
 
+session_start();
+
 // Database Initialization
 $sql = "CREATE DATABASE old_home";
 if ($conn->query($sql) === TRUE) {
@@ -111,7 +113,7 @@ if ($conn->query($sql) === TRUE) {
     // Prescription Table
     $sql_prescription_table = "CREATE TABLE prescription (patient_id int PRIMARY KEY, doctorid int, FOREIGN KEY (doctorid) REFERENCES doctor_appt(doctorid), appt_exist boolean);";
 
-
+    
 
     // Sample Data
 
@@ -129,20 +131,20 @@ if ($conn->query($sql) === TRUE) {
     (23, 'caregiver', 'izzy', 'rodriguez', '1092837465', '2011-12-28', 'cg2@a.com', '1', 1),
     (24, 'caregiver', 'qincy', 'ruze', '657483892', '2012-03-27', 'cg3@a.com', '1', 1),
     (25, 'caregiver', 'prince', 'op', '1', '2017-10-30', 'cg4@a.com', '1', 1);";
-
+    
   $sql_default_security = "INSERT INTO role (page, admin, patient, family, doctor, supervisor, caregiver)
     VALUES
-    ('adminreport', 1, 0, 0, 0, 0, 0),
-    ('role', 1, 0, 0, 0, 0, 0),
-    ('caregiverhome', 0, 0, 0, 0, 0, 1),
-    ('doctorhome', 0, 0, 0, 1, 0, 0),
-    ('familyhome', 0, 0, 1, 0, 0, 0),
-    ('patienthome', 0, 1, 0, 0, 0, 0),
-    ('supervisorhome', 0, 0, 0, 0, 1, 0),
-    ('payment', 1, 0, 0, 0, 1, 0),
-    ('newroster', 1, 0, 0, 0, 1, 0),
-    ('register', 1, 0, 0, 0, 1, 0),
-    ('regapproval', 1, 0, 0, 0, 1, 0);";
+    ('adminreport.php', 1, 0, 0, 0, 0, 0),
+    ('role.php', 1, 0, 0, 0, 0, 0),
+    ('caregiverhome.php', 0, 0, 0, 0, 0, 1),
+    ('doctorhome.php', 0, 0, 0, 1, 0, 0),
+    ('familyhome.php', 0, 0, 1, 0, 0, 0),
+    ('patienthome.php', 0, 1, 0, 0, 0, 0),
+    ('supervisorhome.php', 0, 0, 0, 0, 1, 0),
+    ('payment.php', 1, 0, 0, 0, 1, 0),
+    ('newroster.php', 1, 0, 0, 0, 1, 0),
+    ('register.php', 1, 0, 0, 0, 1, 0),
+    ('regapproval.php', 1, 0, 0, 0, 1, 0);";
 
     $sql_patient_data = ("INSERT INTO `patient` (`userid`, `patientid`, `family_code`, `emergency_contact_number`, `relation`, `group_num`, `admission_date`, `amount_due`) VALUES
     (15, 15, 54, '90876543', 'mom', 4444, '2019-11-22', 40000),
@@ -158,13 +160,8 @@ if ($conn->query($sql) === TRUE) {
 
     $sql_employee_data = "INSERT INTO `employee` (`userid`, `salary`) VALUES ('14', '100000'), ('25', '20000'), ('24', '20000'), ('23', '20000'), ('22', '20000'), ('20', '30000'), ('19', '30000'), ('13', '30000'), ('21', '25000');";
     // SET GLOBAL EVENT SCHEDULER ON;
-
-    $sql_roster_data = "INSERT INTO roster (roster_date, supervisor, doctor, caregiver_1, caregiver_group1, caregiver_2, caregiver_group2, caregiver_3, caregiver_group3, caregiver_4, caregiver_group4) VALUES
-    ('2019-11-25', 21, 13, 22, 1, 23, 2, 24, 3, 25, 4),
-    ('2019-11-26', 21, 19, 22, 4, 23, 1, 24, 2, 25, 3),
-    ('2019-11-27', 21, 20, 22, 3, 23, 4, 24, 2, 25, 1);";
     $sql_scheduler = "SET GLOBAL event_scheduler = 1;";
-
+ 
     $result = mysqli_query($conn, $sql_scheduler);
     $result = mysqli_query($conn, $sql_user);
     $result = mysqli_query($conn, $sql_role_table);
@@ -179,7 +176,6 @@ if ($conn->query($sql) === TRUE) {
     $result = mysqli_query($conn, $sql_patient_data);
     $result = mysqli_query($conn, $sql_doct_appt_data);
     $result = mysqli_query($conn, $sql_employee_data);
-    $result = mysqli_query($conn,$sql_roster_data);
 
 } else {}
 
@@ -194,15 +190,15 @@ $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 
 // Security
 function securitygate($conn) {
-    $currentpage = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
+    $currentpage = basename($_SERVER['PHP_SELF']);
     $sessionrole = $_SESSION['role'];
     $securitycheck = "SELECT $sessionrole FROM role WHERE page = '$currentpage'";
     $clearance = mysqli_query($conn, $securitycheck);
     $passclearance = mysqli_fetch_assoc($clearance);
     if ($passclearance[$sessionrole] == 1) {
     } else {
-        $_SESSION['message'] = "You are not authorized to visit {$currentpage}, you have been logged out.";
-        header("Location: /finalproject220/index.php");
+        $_SESSION['message'] = 'You are not authorized to visit that page, you have been logged out.';
+        header("Location: index.php");
     }
 }
 
