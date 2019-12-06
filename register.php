1b382aaf32e6111023d44f86f9b9b31b63d75c0f
Jonathan Marciano <bobthemoon@gmail.com>
2019-12-06 08:36:09 -0500

    All form fields on reg and index required

--- register.php
+++ register.php
@@ -38,22 +38,22 @@ include_once 'db.php';
             </select>
             <br>
             <label>First Name: </label>
-            <input type="text" name="fname">
+            <input type="text" name="fname" required>
             <br>
             <label>Last Name: </label>
-            <input type="text" name="lname">
+            <input type="text" name="lname" required>
             <br>
             <label>Email: </label>
-            <input type="text" name="email">
+            <input type="text" name="email" required>
             <br>
             <label>Password: </label>
-            <input type="password" name="pass">
+            <input type="password" name="pass" required>
             <br>
             <label>Phone: </label>
-            <input type="text" name="phone">
+            <input type="text" name="phone" required>
             <br>
             <label>Date of Birth: </label>
-            <input type="date" name="dob">
+            <input type="date" name="dob" required>
             <br>
             <!-- Make this appear when role is patient -->
             <section id="section" style='display:none'>
@@ -112,6 +112,7 @@ if (!empty($_POST)) {
     $emcontact = $_POST['$emcontact'] ?? '';
     $relation = $_POST['relation'] ?? '';
 
+    if (!empty($fname) and !empty($lname) and !empty($email) and !empty($phone) and !empty($dob) and !empty($pass)) {
     $usersinfo = "INSERT INTO users (role, fname, lname, phone, dob, email, pass, approved)
 VALUES ('$role', '$fname', '$lname', '$phone', '$dob', '$email', '$pass', 0);";
     $what = mysqli_query($conn, $usersinfo);
@@ -128,6 +129,10 @@ VALUES ('$role', '$fname', '$lname', '$phone', '$dob', '$email', '$pass', 0);";
         $salary = "INSERT INTO employee(userid)
         VALUES ('$youid');";
         mysqli_query($conn, $salary);
+    }
+}
+    else {
+        echo "Please fill in all inputs before submitting";
     }
 }
 ?>
