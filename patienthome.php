959ccadfce5a17f8b26f6c881d3f594eeb9c5d87
Jonathan Marciano <bobthemoon@gmail.com>
2019-12-09 09:28:15 -0500

    patient home fix part 1

--- patienthome.php
+++ patienthome.php
@@ -18,9 +18,16 @@ securitygate($conn);
         <label>Date: </label>
         <input type="date" name="date" value="">
         <label>Patient Name:
-            <!-- Session username here --></label>
+            <?php
+            $theid = $_SESSION['id'] ;
+            $sql = "SELECT fname, lname FROM users WHERE userid = '$theid';";
+            $nameis = mysqli_query($conn, $sql);
+            $havename = mysqli_fetch_assoc($nameis);
+            echo ucfirst($havename['fname']) . ' ' . ucfirst($havename['lname']);?>
+        </label>
         <label>Patient ID:
-            <!-- Session patientid here --></label>
+            <?php
+            echo $_SESSION['id'];?></label>
 
         <br>
         <form action="" method="post">
@@ -31,21 +38,30 @@ securitygate($conn);
                 <tr>
                     <th>Doctor</th>
                     <th>Appointment</th>
-                    <th>Caregiver</th>
                     <th>Morning Medicine</th>
                     <th>Afternoon Medicine</th>
                     <th>Night Medicine</th>
-                    <th>Breakfast</th>
-                    <th>Lunch</th>
-                    <th>Dinner</th>
                 </tr>
-
-            </table>
-
-
-
-
-        </form>
+<?php               $sql_search = "SELECT DISTINCT CONCAT(u.fname, ' ', u.lname) AS name, d.doctorid, d.apt_date, d.morning_med, d.afternoon_med, d.night_med FROM users u JOIN doctor_appt d ON u.userid = d.patientid WHERE d.patientid = '$theid';";
+                    $name_query = mysqli_query($conn, $sql_search);
+                    if (mysqli_num_rows($name_query) > 0) {
+                        while ($row = mysqli_fetch_assoc($name_query)) {
+                            $doctorid = $row['doctorid'];
+                            $getit = "SELECT CONCAT(fname, ' ', lname) AS name FROM users WHERE userid = '$doctorid';";
+                            $docname_query = mysqli_query($conn, $getit);
+                            $nameofdoc = mysqli_fetch_assoc($docname_query);
+                            echo "<tr>";
+                            echo "<td>{$nameofdoc['name']}</td>";
+                            echo "<td>{$row['apt_date']}</td>";
+                            echo "<td>{$row['morning_med']}</td>";
+                            echo "<td>{$row['afternoon_med']}</td>";
+                            echo "<td>{$row['night_med']}</td>";
+                            echo "</tr>";
+                        }
+                    }
+                    ?>
+        </table>
+    </form>
     </main>
 
 
